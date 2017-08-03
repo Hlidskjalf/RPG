0758ca8ffae4415ae17ac165ec99472c7c992cf5
MrShutCo <tylersafaric99@gmail.com>
2017-08-03 14:53:47 -0400

    Added level importer/character movement
    Can import prefab from tiled, and character now moves fluidly

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target; //This will be your citizen
+    public float distance;
+
+
+    void Update() {
+        if (!target) {
+            // Search for object with Player tag
+            var go = GameObject.FindWithTag("Player");
+            // Check we found an object with the player tag
+            if (go)
+                // Set the target to the object we found
+                target = go.transform;
+        }
+
+        if (target)
+            transform.position = new Vector3(target.position.x, target.position.y, target.position.z - distance);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    float isGroundedRayLength = 0.1f;
+    public LayerMask layerMaskForGrounded;
+    public float speed;
+    public float jumpSpeed;
+    public float maxSpeed;
+    public bool isGrounded
+    {
+        get
+        {
+            Vector3 position = transform.position;
+            position.y = GetComponent<Collider2D>().bounds.min.y + 0.1f;
+            float length = isGroundedRayLength + 0.1f;
+            Debug.DrawRay(position, Vector3.down * length);
+            bool grounded = Physics2D.Raycast(position, Vector3.down, length, layerMaskForGrounded.value);
+            return grounded;
+        }
+    }
+    Rigidbody2D rb;
+    Vector3 jump = new Vector3(0.0f, 2.0f, 0.0f);
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.AddForce(new Vector3(moveHorizontal, 0) * speed, ForceMode2D.Impulse);
+        if (Mathf.Abs(rb.velocity.x) >= maxSpeed) {
+            rb.AddForce(new Vector3(moveHorizontal, 0) * -(Mathf.Abs(rb.velocity.x) - maxSpeed), ForceMode2D.Impulse);
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            rb.AddForce(jump * jumpSpeed, ForceMode2D.Impulse);
+        }
+        //Make sure we can max out speed
+        
+    }
+}
