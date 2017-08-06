83ed489d014bddb1aff41b86d112f4451b672b56
Andrew <vroman197@gmail.com>
2017-08-06 17:40:56 -0400

    Teleportation to cave

---
+++ Assets/Scripts/ReturnFromCave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnFromCave : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+		player.transform.position = new Vector2(3646, -123);
+	}
+}
