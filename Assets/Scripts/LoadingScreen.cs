da823782c8cb9a6b39007bef8522149423a4f554
Hlidskjalf <vroman197@gmail.com>
2017-08-19 14:54:49 -0400

    Added Loading Scene

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D collider) 
+     {
+     if(collider.tag == "Player")
+     Application.LoadLevel("Loading");
+     }
+}
