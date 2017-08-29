5c860a98cd8f183b611ad7d42dcbc120bc70495a
Hlidskjalf <vroman197@gmail.com>
2017-08-29 09:24:00 -0400

    Joystick controls

---
+++ Assets/Scripts/MobileController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MobileController : MonoBehaviour 
+{
+	Rigidbody2D player;
+	public float moveForce = 5;
+
+	void Start () 
+	{
+		player = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	void FixedUpdate () 
+	{
+		Vector2 moveVec = new Vector2 (CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+
+		player.AddForce (moveVec);
+	}
+}
